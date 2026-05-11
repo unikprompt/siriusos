clearworks-ai/cortextos

feat(ops): add optional self-healing watchdog scripts under scripts/self-healing/ (#327)
Adds three opt-in scripts that auto-recover a cortextOS fleet from common
failure modes documented in #296 (BUG-011 cascade), #326 (PTY vision hang),
and Telegram poller wedges. Each runs via launchd on a 5-30min cycle.

- watchdog.sh: daemon-level Telegram poller wedge detector. Restarts daemon
  via PM2 when error count exceeds threshold (default 150) in last 5 min.
- agent-recover.sh: per-agent silence detector. Restarts JUST the hung agent
  (no daemon-wide restart) when stdout idle 6+ min while daemon is still
  injecting. 20-min per-agent cooldown.
- usage-monitor.sh: Claude Code burn-rate watcher via ccusage. Telegram
  alerts on tier transitions (GREEN<$15, YELLOW $15-30, RED >$30).

Production-tested over 18 hours on a 7-agent / 2-org fleet at v0.1.1:
8 watchdog fires, 37 auto-recoveries, all healthy. Without these scripts
the fleet would have died overnight.

Stop-gaps, not fixes. Linked to upstream issues so they can be removed
once the underlying bugs are addressed.

No source changes — purely additive under scripts/self-healing/.
Auto-detects alert bot from first enabled orchestrator's .env; override
with CORTEXTOS_ALERT_BOT_ENV. Tunables exposed as env vars.

Co-authored-by: Bobby Shiffler <bobby.shiffler@gmail.com>

scripts/self-healing/agent-recover.sh
@@ -1,0 +1,114 @@
+#!/bin/bash
+# Per-agent silence watchdog for cortextOS.
+# Detects agents whose process is alive but stuck (stdout idle while daemon is
+# still injecting messages), and restarts JUST that agent — not the whole
+# daemon. Avoids the cascade restart pattern that triggers BUG-011 (#296).
+#
+# Runs every 5 minutes via launchd. See README.md for install instructions.
+
+set -u
+
+INSTANCE="${CTX_INSTANCE_ID:-default}"
+FRAMEWORK_ROOT="${CTX_FRAMEWORK_ROOT:-$HOME/cortextos}"
+DAEMON_LOG="${PM2_HOME:-$HOME/.pm2}/logs/cortextos-daemon-out.log"
+LOG_FILE="$HOME/.cortextos/$INSTANCE/logs/agent-recover.log"
+STATE_DIR="$HOME/.cortextos/$INSTANCE/agent-recover-state"
+
+# Tunables
+IDLE_THRESHOLD_SEC="${AGENT_IDLE_THRESHOLD_SEC:-360}"   # 6 minutes
+COOLDOWN_SEC="${AGENT_COOLDOWN_SEC:-1200}"              # 20 minutes per-agent
+
+JQ_BIN="$(command -v jq)"
+[ -z "$JQ_BIN" ] && { echo "agent-recover: jq not on PATH" >&2; exit 1; }
+
+# Auto-detect alert bot. Override with CORTEXTOS_ALERT_BOT_ENV=/path/to/.env
+ALERT_BOT_ENV="${CORTEXTOS_ALERT_BOT_ENV:-}"
+if [ -z "$ALERT_BOT_ENV" ]; then
+  # Find first enabled orchestrator's .env via context.json
+  for ctx in "$FRAMEWORK_ROOT"/orgs/*/context.json; do
+    [ -f "$ctx" ] || continue
+    orch=$("$JQ_BIN" -r '.orchestrator // empty' "$ctx")
+    [ -z "$orch" ] && continue
+    org=$(basename "$(dirname "$ctx")")
+    candidate="$FRAMEWORK_ROOT/orgs/$org/agents/$orch/.env"
+    [ -f "$candidate" ] && ALERT_BOT_ENV="$candidate" && break
+  done
+fi
+
+BOT_TOKEN=""
+CHAT_ID=""
+if [ -n "$ALERT_BOT_ENV" ] && [ -f "$ALERT_BOT_ENV" ]; then
+  BOT_TOKEN=$(grep -E "^BOT_TOKEN=" "$ALERT_BOT_ENV" 2>/dev/null | cut -d= -f2)
+  CHAT_ID=$(grep -E "^CHAT_ID=" "$ALERT_BOT_ENV" 2>/dev/null | cut -d= -f2)
+fi
+
+mkdir -p "$STATE_DIR" "$(dirname "$LOG_FILE")"
+ts="$(date '+%Y-%m-%d %H:%M:%S')"
+now=$(date +%s)
+
+# Gather enabled agents from registry
+enabled_json="$HOME/.cortextos/$INSTANCE/config/enabled-agents.json"
+[ -f "$enabled_json" ] || { echo "[$ts] enabled-agents.json missing — skip" >> "$LOG_FILE"; exit 0; }
+
+agents=$("$JQ_BIN" -r 'to_entries[] | select(.value.enabled == true) | .key' "$enabled_json")
+
+# Pull recent daemon log once (avoid re-reading per agent)
+recent_log=$(tail -300 "$DAEMON_LOG" 2>/dev/null)
+
+restarted=()
+
+for agent in $agents; do
+  stdout="$HOME/.cortextos/$INSTANCE/logs/$agent/stdout.log"
+  [ -f "$stdout" ] || continue
+
+  # PID via cortextos status
+  pid=$(node "$FRAMEWORK_ROOT/dist/cli.js" status --instance "$INSTANCE" 2>/dev/null \
+        | awk -v a="$agent" '$1 == a { print $3 }')
+  [ -z "$pid" ] || [ "$pid" = "-" ] && continue
+
+  # Process actually alive?
+  kill -0 "$pid" 2>/dev/null || continue
+
+  # Idle time for stdout
+  stdout_mtime=$(stat -f "%m" "$stdout" 2>/dev/null || stat -c "%Y" "$stdout" 2>/dev/null)
+  [ -z "$stdout_mtime" ] && continue
+  idle=$((now - stdout_mtime))
+  [ "$idle" -lt "$IDLE_THRESHOLD_SEC" ] && continue
+
+  # Did the daemon inject anything for this agent recently?
+  echo "$recent_log" | grep -q "\[$agent\] Injected" || continue
+
+  # Cooldown check
+  cooldown_file="$STATE_DIR/$agent.last-restart"
+  if [ -f "$cooldown_file" ]; then
+    last=$(cat "$cooldown_file")
+    if [ $((now - last)) -lt "$COOLDOWN_SEC" ]; then
+      echo "[$ts] $agent: hung (idle ${idle}s) but in cooldown — skip" >> "$LOG_FILE"
+      continue
+    fi
+  fi
+
+  # === Restart this agent ===
+  echo "[$ts] $agent: HUNG — pid=$pid alive, stdout idle ${idle}s, daemon has injected messages. Restarting." >> "$LOG_FILE"
+  node "$FRAMEWORK_ROOT/dist/cli.js" stop "$agent" --instance "$INSTANCE" >> "$LOG_FILE" 2>&1
+  sleep 2
+  node "$FRAMEWORK_ROOT/dist/cli.js" start "$agent" --instance "$INSTANCE" >> "$LOG_FILE" 2>&1
+  echo "$now" > "$cooldown_file"
+  restarted+=("$agent")
+done
+
+# Single Telegram alert if anything was restarted
+if [ "${#restarted[@]}" -gt 0 ] && [ -n "$BOT_TOKEN" ] && [ -n "$CHAT_ID" ]; then
+  list=$(printf ', %s' "${restarted[@]}")
+  list=${list:2}
+  msg="🔧 *Agent auto-recover*
+Hung agents restarted: *${list}*
+(Process was alive but stdout idle ≥ ${IDLE_THRESHOLD_SEC}s while messages queued. Auto-recovered without daemon-wide restart.)"
+
+  curl -sS --max-time 10 "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" \
+    -H "Content-Type: application/json" \
+    -d "$(printf '{"chat_id": %s, "text": %s, "parse_mode": "Markdown"}' "$CHAT_ID" "$(echo "$msg" | "$JQ_BIN" -Rs .)")" \
+    >> "$LOG_FILE" 2>&1
+fi
+
+[ "${#restarted[@]}" -eq 0 ] && echo "[$ts] OK: all enabled agents healthy" >> "$LOG_FILE"

scripts/self-healing/usage-monitor.sh
@@ -1,0 +1,134 @@
+#!/bin/bash
+# Claude Code usage monitor for cortextOS.
+# Computes current 5-hour Claude Code session-block burn rate from ccusage and
+# Telegram-alerts on tier transitions. Fires alerts only on entering a new
+# tier (or while in RED) — won't spam.
+#
+# Tiers (tunable below):
+#   GREEN  : < $15/hr        — silent, log only
+#   YELLOW : $15–$30/hr      — Telegram alert (once per state transition)
+#   RED    : > $30/hr        — Telegram alert (every check while elevated)
+#
+# Runs every 30 minutes via launchd. See README.md for install instructions.
+
+set -u
+
+INSTANCE="${CTX_INSTANCE_ID:-default}"
+FRAMEWORK_ROOT="${CTX_FRAMEWORK_ROOT:-$HOME/cortextos}"
+STATE_FILE="$HOME/.cortextos/$INSTANCE/usage-monitor-state"
+LOG_FILE="$HOME/.cortextos/$INSTANCE/logs/usage-monitor.log"
+
+# Thresholds (USD/hr)
+YELLOW_THRESHOLD="${USAGE_YELLOW_THRESHOLD:-15}"
+RED_THRESHOLD="${USAGE_RED_THRESHOLD:-30}"
+
+CCUSAGE_BIN="$(command -v ccusage)"
+JQ_BIN="$(command -v jq)"
+[ -z "$CCUSAGE_BIN" ] && { echo "usage-monitor: ccusage not on PATH (npm install -g ccusage)" >&2; exit 1; }
+[ -z "$JQ_BIN" ] && { echo "usage-monitor: jq not on PATH" >&2; exit 1; }
+
+# Auto-detect alert bot. Override with CORTEXTOS_ALERT_BOT_ENV=/path/to/.env
+ALERT_BOT_ENV="${CORTEXTOS_ALERT_BOT_ENV:-}"
+if [ -z "$ALERT_BOT_ENV" ]; then
+  for ctx in "$FRAMEWORK_ROOT"/orgs/*/context.json; do
+    [ -f "$ctx" ] || continue
+    orch=$("$JQ_BIN" -r '.orchestrator // empty' "$ctx")
+    [ -z "$orch" ] && continue
+    org=$(basename "$(dirname "$ctx")")
+    candidate="$FRAMEWORK_ROOT/orgs/$org/agents/$orch/.env"
+    [ -f "$candidate" ] && ALERT_BOT_ENV="$candidate" && break
+  done
+fi
+
+BOT_TOKEN=""
+CHAT_ID=""
+if [ -n "$ALERT_BOT_ENV" ] && [ -f "$ALERT_BOT_ENV" ]; then
+  BOT_TOKEN=$(grep -E "^BOT_TOKEN=" "$ALERT_BOT_ENV" 2>/dev/null | cut -d= -f2)
+  CHAT_ID=$(grep -E "^CHAT_ID=" "$ALERT_BOT_ENV" 2>/dev/null | cut -d= -f2)
+fi
+
+mkdir -p "$(dirname "$LOG_FILE")"
+ts="$(date '+%Y-%m-%d %H:%M:%S')"
+
+# Pull ccusage blocks JSON, isolate the active block
+blocks_json=$("$CCUSAGE_BIN" blocks --json 2>/dev/null)
+active=$(echo "$blocks_json" | "$JQ_BIN" -c '.blocks | map(select(.isActive == true)) | .[0] // empty')
+
+if [ -z "$active" ]; then
+  echo "[$ts] No active block — fleet is idle. Skipping check." >> "$LOG_FILE"
+  exit 0
+fi
+
+cost=$(echo "$active" | "$JQ_BIN" -r '.costUSD')
+start_iso=$(echo "$active" | "$JQ_BIN" -r '.startTime')
+# Strip the .NNNZ suffix and parse as UTC
+start_unix=""
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  start_unix=$(date -j -u -f "%Y-%m-%dT%H:%M:%S" "${start_iso%.*}" +%s 2>/dev/null)
+else
+  start_unix=$(date -u -d "${start_iso}" +%s 2>/dev/null)
+fi
+now_unix=$(date +%s)
+if [ -n "$start_unix" ]; then
+  elapsed_min=$(( (now_unix - start_unix) / 60 ))
+else
+  elapsed_min=0
+fi
+projected_cost=$(echo "$active" | "$JQ_BIN" -r '.projection.totalCost // empty')
+
+# Burn rate $/hr
+if [ "$elapsed_min" -gt 0 ]; then
+  rate=$(echo "$cost $elapsed_min" | awk '{printf "%.2f", ($1 * 60) / $2}')
+else
+  rate="0.00"
+fi
+
+# Determine current tier (use awk for portable float compare)
+tier=$(awk -v r="$rate" -v y="$YELLOW_THRESHOLD" -v R="$RED_THRESHOLD" \
+  'BEGIN { if (r >= R) print "RED"; else if (r >= y) print "YELLOW"; else print "GREEN" }')
+
+last_tier=$(cat "$STATE_FILE" 2>/dev/null || echo "GREEN")
+echo "$tier" > "$STATE_FILE"
+
+proj_str=""
+[ -n "$projected_cost" ] && proj_str=" (projected block total: \$${projected_cost})"
+echo "[$ts] tier=$tier rate=\$${rate}/hr cost=\$${cost} elapsed=${elapsed_min}m${proj_str}" >> "$LOG_FILE"
+
+# Alert on tier change OR every check while RED
+should_alert=0
+[ "$tier" != "$last_tier" ] && should_alert=1
+[ "$tier" = "RED" ] && should_alert=1
+
+if [ "$should_alert" -eq 1 ] && [ -n "$BOT_TOKEN" ] && [ -n "$CHAT_ID" ]; then
+  case "$tier" in
+    RED)    icon="🔴" ;;
+    YELLOW) icon="🟡" ;;
+    GREEN)  icon="🟢" ;;
+  esac
+
+  msg="${icon} *Usage Monitor — ${tier}*
+Burn rate: *\$${rate}/hr*
+Active block: \$${cost} so far over ${elapsed_min}m"
+
+  [ -n "$projected_cost" ] && msg="${msg}
+Projected for full 5h block: *\$${projected_cost}*"
+
+  if [ "$tier" = "GREEN" ] && [ "$last_tier" != "GREEN" ]; then
+    msg="${msg}
+
+✅ Recovered to safe burn. (Was: ${last_tier})"
+  elif [ "$tier" = "YELLOW" ]; then
+    msg="${msg}
+
+Watching. Run \`ccusage blocks\` for breakdown."
+  elif [ "$tier" = "RED" ]; then
+    msg="${msg}
+
+🚨 High burn. Identify heavy agent (\`ccusage session\`), pause non-critical specialists, or stop the daemon."
+  fi
+
+  curl -sS --max-time 10 "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" \
+    -H "Content-Type: application/json" \
+    -d "$(printf '{"chat_id": %s, "text": %s, "parse_mode": "Markdown"}' "$CHAT_ID" "$(echo "$msg" | "$JQ_BIN" -Rs .)")" \
+    >> "$LOG_FILE" 2>&1
+fi

scripts/self-healing/watchdog.sh
@@ -1,0 +1,47 @@
+#!/bin/bash
+# Daemon-level watchdog for cortextOS.
+# Detects when the Telegram poller is wedged (accumulated fetch failures) and
+# restarts cortextos-daemon via PM2 to clear stuck connections.
+#
+# Runs every 5 minutes via launchd. See README.md for install instructions.
+
+set -u
+
+INSTANCE="${CTX_INSTANCE_ID:-default}"
+ERR_LOG="${PM2_HOME:-$HOME/.pm2}/logs/cortextos-daemon-error.log"
+STATE_FILE="$HOME/.cortextos/$INSTANCE/watchdog-state"
+LOG_FILE="$HOME/.cortextos/$INSTANCE/logs/watchdog.log"
+
+# Threshold: if more than this many new poller-error lines appeared since the
+# last check (≈5 min ago), assume wedge and restart. A healthy daemon produces
+# 0–2 transient poll errors over 5 min; a wedged daemon spams hundreds.
+THRESHOLD="${WATCHDOG_THRESHOLD:-150}"
+
+PM2_BIN="$(command -v pm2)"
+[ -z "$PM2_BIN" ] && { echo "watchdog: pm2 not on PATH" >&2; exit 1; }
+
+mkdir -p "$(dirname "$LOG_FILE")"
+ts="$(date '+%Y-%m-%d %H:%M:%S')"
+
+if [ ! -f "$ERR_LOG" ]; then
+  echo "[$ts] err log missing: $ERR_LOG — skip" >> "$LOG_FILE"
+  exit 0
+fi
+
+# Track only lines containing actual poller failures, not unrelated noise.
+current=$(grep -c "telegram-poller.*Poll error\|fetch failed" "$ERR_LOG" 2>/dev/null || echo 0)
+last=$(cat "$STATE_FILE" 2>/dev/null || echo 0)
+delta=$(( current - last ))
+
+# Handle log rotation (current < last)
+[ "$delta" -lt 0 ] && delta="$current"
+
+if [ "$delta" -gt "$THRESHOLD" ]; then
+  echo "[$ts] WEDGED: $delta new poller errors since last check (threshold $THRESHOLD). Restarting cortextos-daemon." >> "$LOG_FILE"
+  "$PM2_BIN" restart cortextos-daemon --update-env >> "$LOG_FILE" 2>&1
+  # After restart, snapshot the new line count so we don't immediately re-fire.
+  echo "$(grep -c "telegram-poller.*Poll error\|fetch failed" "$ERR_LOG" 2>/dev/null || echo 0)" > "$STATE_FILE"
+else
+  echo "[$ts] OK: $delta new poller errors (threshold $THRESHOLD)." >> "$LOG_FILE"
+  echo "$current" > "$STATE_FILE"
+fi

unikprompt/siriusos

scripts/self-healing/agent-recover.sh
@@ -1,0 +1,114 @@
+#!/bin/bash
+# Per-agent silence watchdog for SiriusOS.
+# Detects agents whose process is alive but stuck (stdout idle while daemon is
+# still injecting messages), and restarts JUST that agent — not the whole
+# daemon. Avoids the cascade restart pattern that triggers BUG-011 (#296).
+#
+# Runs every 5 minutes via launchd. See README.md for install instructions.
+
+set -u
+
+INSTANCE="${CTX_INSTANCE_ID:-default}"
+FRAMEWORK_ROOT="${CTX_FRAMEWORK_ROOT:-$HOME/siriusos}"
+DAEMON_LOG="${PM2_HOME:-$HOME/.pm2}/logs/siriusos-daemon-out.log"
+LOG_FILE="$HOME/.siriusos/$INSTANCE/logs/agent-recover.log"
+STATE_DIR="$HOME/.siriusos/$INSTANCE/agent-recover-state"
+
+# Tunables
+IDLE_THRESHOLD_SEC="${AGENT_IDLE_THRESHOLD_SEC:-360}"   # 6 minutes
+COOLDOWN_SEC="${AGENT_COOLDOWN_SEC:-1200}"              # 20 minutes per-agent
+
+JQ_BIN="$(command -v jq)"
+[ -z "$JQ_BIN" ] && { echo "agent-recover: jq not on PATH" >&2; exit 1; }
+
+# Auto-detect alert bot. Override with SIRIUSOS_ALERT_BOT_ENV=/path/to/.env
+ALERT_BOT_ENV="${SIRIUSOS_ALERT_BOT_ENV:-}"
+if [ -z "$ALERT_BOT_ENV" ]; then
+  # Find first enabled orchestrator's .env via context.json
+  for ctx in "$FRAMEWORK_ROOT"/orgs/*/context.json; do
+    [ -f "$ctx" ] || continue
+    orch=$("$JQ_BIN" -r '.orchestrator // empty' "$ctx")
+    [ -z "$orch" ] && continue
+    org=$(basename "$(dirname "$ctx")")
+    candidate="$FRAMEWORK_ROOT/orgs/$org/agents/$orch/.env"
+    [ -f "$candidate" ] && ALERT_BOT_ENV="$candidate" && break
+  done
+fi
+
+BOT_TOKEN=""
+CHAT_ID=""
+if [ -n "$ALERT_BOT_ENV" ] && [ -f "$ALERT_BOT_ENV" ]; then
+  BOT_TOKEN=$(grep -E "^BOT_TOKEN=" "$ALERT_BOT_ENV" 2>/dev/null | cut -d= -f2)
+  CHAT_ID=$(grep -E "^CHAT_ID=" "$ALERT_BOT_ENV" 2>/dev/null | cut -d= -f2)
+fi
+
+mkdir -p "$STATE_DIR" "$(dirname "$LOG_FILE")"
+ts="$(date '+%Y-%m-%d %H:%M:%S')"
+now=$(date +%s)
+
+# Gather enabled agents from registry
+enabled_json="$HOME/.siriusos/$INSTANCE/config/enabled-agents.json"
+[ -f "$enabled_json" ] || { echo "[$ts] enabled-agents.json missing — skip" >> "$LOG_FILE"; exit 0; }
+
+agents=$("$JQ_BIN" -r 'to_entries[] | select(.value.enabled == true) | .key' "$enabled_json")
+
+# Pull recent daemon log once (avoid re-reading per agent)
+recent_log=$(tail -300 "$DAEMON_LOG" 2>/dev/null)
+
+restarted=()
+
+for agent in $agents; do
+  stdout="$HOME/.siriusos/$INSTANCE/logs/$agent/stdout.log"
+  [ -f "$stdout" ] || continue
+
+  # PID via siriusos status
+  pid=$(node "$FRAMEWORK_ROOT/dist/cli.js" status --instance "$INSTANCE" 2>/dev/null \
+        | awk -v a="$agent" '$1 == a { print $3 }')
+  [ -z "$pid" ] || [ "$pid" = "-" ] && continue
+
+  # Process actually alive?
+  kill -0 "$pid" 2>/dev/null || continue
+
+  # Idle time for stdout
+  stdout_mtime=$(stat -f "%m" "$stdout" 2>/dev/null || stat -c "%Y" "$stdout" 2>/dev/null)
+  [ -z "$stdout_mtime" ] && continue
+  idle=$((now - stdout_mtime))
+  [ "$idle" -lt "$IDLE_THRESHOLD_SEC" ] && continue
+
+  # Did the daemon inject anything for this agent recently?
+  echo "$recent_log" | grep -q "\[$agent\] Injected" || continue
+
+  # Cooldown check
+  cooldown_file="$STATE_DIR/$agent.last-restart"
+  if [ -f "$cooldown_file" ]; then
+    last=$(cat "$cooldown_file")
+    if [ $((now - last)) -lt "$COOLDOWN_SEC" ]; then
+      echo "[$ts] $agent: hung (idle ${idle}s) but in cooldown — skip" >> "$LOG_FILE"
+      continue
+    fi
+  fi
+
+  # === Restart this agent ===
+  echo "[$ts] $agent: HUNG — pid=$pid alive, stdout idle ${idle}s, daemon has injected messages. Restarting." >> "$LOG_FILE"
+  node "$FRAMEWORK_ROOT/dist/cli.js" stop "$agent" --instance "$INSTANCE" >> "$LOG_FILE" 2>&1
+  sleep 2
+  node "$FRAMEWORK_ROOT/dist/cli.js" start "$agent" --instance "$INSTANCE" >> "$LOG_FILE" 2>&1
+  echo "$now" > "$cooldown_file"
+  restarted+=("$agent")
+done
+
+# Single Telegram alert if anything was restarted
+if [ "${#restarted[@]}" -gt 0 ] && [ -n "$BOT_TOKEN" ] && [ -n "$CHAT_ID" ]; then
+  list=$(printf ', %s' "${restarted[@]}")
+  list=${list:2}
+  msg="🔧 *Agent auto-recover*
+Hung agents restarted: *${list}*
+(Process was alive but stdout idle ≥ ${IDLE_THRESHOLD_SEC}s while messages queued. Auto-recovered without daemon-wide restart.)"
+
+  curl -sS --max-time 10 "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" \
+    -H "Content-Type: application/json" \
+    -d "$(printf '{"chat_id": %s, "text": %s, "parse_mode": "Markdown"}' "$CHAT_ID" "$(echo "$msg" | "$JQ_BIN" -Rs .)")" \
+    >> "$LOG_FILE" 2>&1
+fi
+
+[ "${#restarted[@]}" -eq 0 ] && echo "[$ts] OK: all enabled agents healthy" >> "$LOG_FILE"

scripts/self-healing/usage-monitor.sh
@@ -1,0 +1,134 @@
+#!/bin/bash
+# Claude Code usage monitor for SiriusOS.
+# Computes current 5-hour Claude Code session-block burn rate from ccusage and
+# Telegram-alerts on tier transitions. Fires alerts only on entering a new
+# tier (or while in RED) — won't spam.
+#
+# Tiers (tunable below):
+#   GREEN  : < $15/hr        — silent, log only
+#   YELLOW : $15–$30/hr      — Telegram alert (once per state transition)
+#   RED    : > $30/hr        — Telegram alert (every check while elevated)
+#
+# Runs every 30 minutes via launchd. See README.md for install instructions.
+
+set -u
+
+INSTANCE="${CTX_INSTANCE_ID:-default}"
+FRAMEWORK_ROOT="${CTX_FRAMEWORK_ROOT:-$HOME/siriusos}"
+STATE_FILE="$HOME/.siriusos/$INSTANCE/usage-monitor-state"
+LOG_FILE="$HOME/.siriusos/$INSTANCE/logs/usage-monitor.log"
+
+# Thresholds (USD/hr)
+YELLOW_THRESHOLD="${USAGE_YELLOW_THRESHOLD:-15}"
+RED_THRESHOLD="${USAGE_RED_THRESHOLD:-30}"
+
+CCUSAGE_BIN="$(command -v ccusage)"
+JQ_BIN="$(command -v jq)"
+[ -z "$CCUSAGE_BIN" ] && { echo "usage-monitor: ccusage not on PATH (npm install -g ccusage)" >&2; exit 1; }
+[ -z "$JQ_BIN" ] && { echo "usage-monitor: jq not on PATH" >&2; exit 1; }
+
+# Auto-detect alert bot. Override with SIRIUSOS_ALERT_BOT_ENV=/path/to/.env
+ALERT_BOT_ENV="${SIRIUSOS_ALERT_BOT_ENV:-}"
+if [ -z "$ALERT_BOT_ENV" ]; then
+  for ctx in "$FRAMEWORK_ROOT"/orgs/*/context.json; do
+    [ -f "$ctx" ] || continue
+    orch=$("$JQ_BIN" -r '.orchestrator // empty' "$ctx")
+    [ -z "$orch" ] && continue
+    org=$(basename "$(dirname "$ctx")")
+    candidate="$FRAMEWORK_ROOT/orgs/$org/agents/$orch/.env"
+    [ -f "$candidate" ] && ALERT_BOT_ENV="$candidate" && break
+  done
+fi
+
+BOT_TOKEN=""
+CHAT_ID=""
+if [ -n "$ALERT_BOT_ENV" ] && [ -f "$ALERT_BOT_ENV" ]; then
+  BOT_TOKEN=$(grep -E "^BOT_TOKEN=" "$ALERT_BOT_ENV" 2>/dev/null | cut -d= -f2)
+  CHAT_ID=$(grep -E "^CHAT_ID=" "$ALERT_BOT_ENV" 2>/dev/null | cut -d= -f2)
+fi
+
+mkdir -p "$(dirname "$LOG_FILE")"
+ts="$(date '+%Y-%m-%d %H:%M:%S')"
+
+# Pull ccusage blocks JSON, isolate the active block
+blocks_json=$("$CCUSAGE_BIN" blocks --json 2>/dev/null)
+active=$(echo "$blocks_json" | "$JQ_BIN" -c '.blocks | map(select(.isActive == true)) | .[0] // empty')
+
+if [ -z "$active" ]; then
+  echo "[$ts] No active block — fleet is idle. Skipping check." >> "$LOG_FILE"
+  exit 0
+fi
+
+cost=$(echo "$active" | "$JQ_BIN" -r '.costUSD')
+start_iso=$(echo "$active" | "$JQ_BIN" -r '.startTime')
+# Strip the .NNNZ suffix and parse as UTC
+start_unix=""
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  start_unix=$(date -j -u -f "%Y-%m-%dT%H:%M:%S" "${start_iso%.*}" +%s 2>/dev/null)
+else
+  start_unix=$(date -u -d "${start_iso}" +%s 2>/dev/null)
+fi
+now_unix=$(date +%s)
+if [ -n "$start_unix" ]; then
+  elapsed_min=$(( (now_unix - start_unix) / 60 ))
+else
+  elapsed_min=0
+fi
+projected_cost=$(echo "$active" | "$JQ_BIN" -r '.projection.totalCost // empty')
+
+# Burn rate $/hr
+if [ "$elapsed_min" -gt 0 ]; then
+  rate=$(echo "$cost $elapsed_min" | awk '{printf "%.2f", ($1 * 60) / $2}')
+else
+  rate="0.00"
+fi
+
+# Determine current tier (use awk for portable float compare)
+tier=$(awk -v r="$rate" -v y="$YELLOW_THRESHOLD" -v R="$RED_THRESHOLD" \
+  'BEGIN { if (r >= R) print "RED"; else if (r >= y) print "YELLOW"; else print "GREEN" }')
+
+last_tier=$(cat "$STATE_FILE" 2>/dev/null || echo "GREEN")
+echo "$tier" > "$STATE_FILE"
+
+proj_str=""
+[ -n "$projected_cost" ] && proj_str=" (projected block total: \$${projected_cost})"
+echo "[$ts] tier=$tier rate=\$${rate}/hr cost=\$${cost} elapsed=${elapsed_min}m${proj_str}" >> "$LOG_FILE"
+
+# Alert on tier change OR every check while RED
+should_alert=0
+[ "$tier" != "$last_tier" ] && should_alert=1
+[ "$tier" = "RED" ] && should_alert=1
+
+if [ "$should_alert" -eq 1 ] && [ -n "$BOT_TOKEN" ] && [ -n "$CHAT_ID" ]; then
+  case "$tier" in
+    RED)    icon="🔴" ;;
+    YELLOW) icon="🟡" ;;
+    GREEN)  icon="🟢" ;;
+  esac
+
+  msg="${icon} *Usage Monitor — ${tier}*
+Burn rate: *\$${rate}/hr*
+Active block: \$${cost} so far over ${elapsed_min}m"
+
+  [ -n "$projected_cost" ] && msg="${msg}
+Projected for full 5h block: *\$${projected_cost}*"
+
+  if [ "$tier" = "GREEN" ] && [ "$last_tier" != "GREEN" ]; then
+    msg="${msg}
+
+✅ Recovered to safe burn. (Was: ${last_tier})"
+  elif [ "$tier" = "YELLOW" ]; then
+    msg="${msg}
+
+Watching. Run \`ccusage blocks\` for breakdown."
+  elif [ "$tier" = "RED" ]; then
+    msg="${msg}
+
+🚨 High burn. Identify heavy agent (\`ccusage session\`), pause non-critical specialists, or stop the daemon."
+  fi
+
+  curl -sS --max-time 10 "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" \
+    -H "Content-Type: application/json" \
+    -d "$(printf '{"chat_id": %s, "text": %s, "parse_mode": "Markdown"}' "$CHAT_ID" "$(echo "$msg" | "$JQ_BIN" -Rs .)")" \
+    >> "$LOG_FILE" 2>&1
+fi

scripts/self-healing/watchdog.sh
@@ -1,0 +1,47 @@
+#!/bin/bash
+# Daemon-level watchdog for SiriusOS.
+# Detects when the Telegram poller is wedged (accumulated fetch failures) and
+# restarts siriusos-daemon via PM2 to clear stuck connections.
+#
+# Runs every 5 minutes via launchd. See README.md for install instructions.
+
+set -u
+
+INSTANCE="${CTX_INSTANCE_ID:-default}"
+ERR_LOG="${PM2_HOME:-$HOME/.pm2}/logs/siriusos-daemon-error.log"
+STATE_FILE="$HOME/.siriusos/$INSTANCE/watchdog-state"
+LOG_FILE="$HOME/.siriusos/$INSTANCE/logs/watchdog.log"
+
+# Threshold: if more than this many new poller-error lines appeared since the
+# last check (≈5 min ago), assume wedge and restart. A healthy daemon produces
+# 0–2 transient poll errors over 5 min; a wedged daemon spams hundreds.
+THRESHOLD="${WATCHDOG_THRESHOLD:-150}"
+
+PM2_BIN="$(command -v pm2)"
+[ -z "$PM2_BIN" ] && { echo "watchdog: pm2 not on PATH" >&2; exit 1; }
+
+mkdir -p "$(dirname "$LOG_FILE")"
+ts="$(date '+%Y-%m-%d %H:%M:%S')"
+
+if [ ! -f "$ERR_LOG" ]; then
+  echo "[$ts] err log missing: $ERR_LOG — skip" >> "$LOG_FILE"
+  exit 0
+fi
+
+# Track only lines containing actual poller failures, not unrelated noise.
+current=$(grep -c "telegram-poller.*Poll error\|fetch failed" "$ERR_LOG" 2>/dev/null || echo 0)
+last=$(cat "$STATE_FILE" 2>/dev/null || echo 0)
+delta=$(( current - last ))
+
+# Handle log rotation (current < last)
+[ "$delta" -lt 0 ] && delta="$current"
+
+if [ "$delta" -gt "$THRESHOLD" ]; then
+  echo "[$ts] WEDGED: $delta new poller errors since last check (threshold $THRESHOLD). Restarting siriusos-daemon." >> "$LOG_FILE"
+  "$PM2_BIN" restart siriusos-daemon --update-env >> "$LOG_FILE" 2>&1
+  # After restart, snapshot the new line count so we don't immediately re-fire.
+  echo "$(grep -c "telegram-poller.*Poll error\|fetch failed" "$ERR_LOG" 2>/dev/null || echo 0)" > "$STATE_FILE"
+else
+  echo "[$ts] OK: $delta new poller errors (threshold $THRESHOLD)." >> "$LOG_FILE"
+  echo "$current" > "$STATE_FILE"
+fi